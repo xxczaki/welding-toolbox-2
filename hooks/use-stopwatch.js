40875ece896efb0c9a09e32f02222e4e4e515307
Antoni Kepinski <xxczaki@pm.me>
2019-12-09 18:12:06 +0100

    useStopwatch hook

---
+++ hooks/use-stopwatch.js
@@ -0,0 +1,30 @@
+import {useState, useEffect, useRef} from 'react';
+
+export const useStopwatch = () => {
+	const [started, setStarted] = useState(false);
+	const [ms, setMs] = useState(0);
+
+	const intervalRef = useRef();
+
+	useEffect(() => {
+		if (started) {
+			const startTime = Date.now() - ms;
+			const id = setInterval(() => {
+				setMs(Date.now() - startTime);
+			}, 1000);
+			intervalRef.current = id;
+		}
+
+		return () => clearInterval(intervalRef.current);
+	});
+	return {
+		ms,
+		isRunning: started,
+		start: () => setStarted(true),
+		stop: () => setStarted(false),
+		reset: () => {
+			setMs(0);
+			setStarted(false);
+		}
+	};
+};
