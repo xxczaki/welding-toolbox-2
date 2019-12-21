cd32903e4dbb9879636350195d2604e314a03720
Antoni Kepinski <xxczaki@pm.me>
2019-12-21 13:52:23 +0100

    Add ButtonBox component

---
+++ components/button-box.js
@@ -0,0 +1,12 @@
+import {View} from 'react-native';
+import styled from 'styled-components';
+
+const ButtonBox = styled(View)`
+    flex-direction: row;
+	align-items: center;
+	justifyContent: center;
+    margin-top: 5px;
+    margin-bottom: 10px;
+`;
+
+export default ButtonBox;
