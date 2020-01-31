1732ce95f4c0a075c12fdcfde45ea657414f836d
Antoni Kepinski <xxczaki@pm.me>
2020-01-31 20:19:14 +0100

    implement async storage for settings

---
+++ storage.js
@@ -0,0 +1,24 @@
+import AsyncStorageFactory from '@react-native-community/async-storage';
+import LegacyStorage from '@react-native-community/async-storage-backend-legacy';
+
+const legacyStorage = new LegacyStorage();
+
+const storage = AsyncStorageFactory.create(legacyStorage);
+
+// If the storage is empty, supply some defaults
+(async () => {
+	const data = await storage.get('settings');
+
+	const defaults = {
+		resultUnit: 'mm',
+		lengthImperial: false,
+		totalEnergy: false,
+		sentryDisable: false
+	};
+
+	if (!data || Object.entries(JSON.parse(data)).length === 0) {
+		storage.set('settings', JSON.stringify(defaults));
+	}
+})();
+
+export default storage;
