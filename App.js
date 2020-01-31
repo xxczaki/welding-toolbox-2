acac92168c1969899bcec94ba15f6444a1c110fd
Antoni Kepinski <xxczaki@pm.me>
2020-01-31 20:18:54 +0100

    Migrate to React Navigation 5

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
+import 'react-native-gesture-handler';
+
 import React, {useEffect} from 'react';
 import {StatusBar} from 'react-native';
 import {DarkTheme, Provider as PaperProvider} from 'react-native-paper';
-import {enableScreens} from 'react-native-screens';
+import {NavigationNativeContainer} from '@react-navigation/native';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import RNBootSplash from 'react-native-bootsplash';
+import * as Sentry from '@sentry/react-native';
+import storage from './storage';
 
-import AppNavigator from './navigation/AppNavigator';
+import TabBarIcon from './components/TabBarIcon';
+
+import HeatInputScreen from './screens/HeatInputScreen';
+import WeldabilityScreen from './screens/WeldabilityScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+storage.get('settings').then(data => {
+	const settings = JSON.parse(data);
+
+	if (!settings?.sentryDisable) {
+		Sentry.init({
+			dsn: 'https://43e9120ad3f34a1b8b2cb7b3b7ca428d@sentry.io/2125584'
+		});
+	}
+});
 
 const theme = {
 	...DarkTheme,
@@ -15,7 +34,7 @@ const theme = {
 	}
 };
 
-enableScreens();
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
 	useEffect(() => {
@@ -23,12 +42,53 @@ export default function App() {
 	}, []);
 
 	return (
-		<PaperProvider theme={theme}>
-			<StatusBar
-				backgroundColor="#212121"
-				barStyle="light-content"
-			/>
-			<AppNavigator/>
-		</PaperProvider>
+		<NavigationNativeContainer>
+			<PaperProvider theme={theme}>
+				<StatusBar
+					backgroundColor="#212121"
+					barStyle="light-content"
+				/>
+				<Tab.Navigator
+					shifting
+					activeColor="#fff"
+					inactiveColor="#ffca47"
+					barStyle={{backgroundColor: '#ff9800'}}
+				>
+					<Tab.Screen
+						name="HeatInput"
+						component={HeatInputScreen}
+						options={{
+							tabBarLabel: 'Heat Input',
+							tabBarColor: '#ff9800',
+							tabBarIcon: ({focused}) => (
+								<TabBarIcon focused={focused} name="fire"/>
+							)
+						}}
+					/>
+					<Tab.Screen
+						name="Weldability"
+						component={WeldabilityScreen}
+						options={{
+							tabBarLabel: 'Weldability',
+							tabBarColor: '#c66900',
+							tabBarIcon: ({focused}) => (
+								<TabBarIcon focused={focused} name="calculator"/>
+							)
+						}}
+					/>
+					<Tab.Screen
+						name="Settings"
+						component={SettingsScreen}
+						options={{
+							tabBarLabel: 'Settings',
+							tabBarColor: '#ffb74d',
+							tabBarIcon: ({focused}) => (
+								<TabBarIcon focused={focused} name="settings-outline"/>
+							)
+						}}
+					/>
+				</Tab.Navigator>
+			</PaperProvider>
+		</NavigationNativeContainer>
 	);
 }
